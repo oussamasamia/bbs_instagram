8435e2fddb9277b4d5614d516408fa1b4abc1d5f
SAMIA Oussama <oussamasamia1@gmail.com>
2023-05-19 00:56:22 +0200

    Get & format Posts from DB

--- app/Services/InstagramPostService.php
+++ app/Services/InstagramPostService.php
@@ -22,8 +22,36 @@ class InstagramPostService
     public function getPosts($limit = 25)
     {
         $jsonData = $this->instagramPostRepository->getAndStoreInstagramPosts($limit);
-
     }
 
+    public function getPostsFromDB($limit = 25)
+    {
+        $posts = $this->instagramPostRepository->getPostsFromDB($limit);
 
+
+        foreach ($posts as $post) {
+            $images = [];
+
+
+            if ($post->media_type === 'CAROUSEL_ALBUM') {
+
+
+                foreach ($post->images as $image) {
+                    $images[] = $image->media_url;
+                }
+
+
+                $post->image_urls = $images;
+
+            } else {
+
+                $images[] = $post->media_url;
+                $post->image_urls = $images;
+
+            }
+        }
+
+        return $posts;
+
+    }
 }
